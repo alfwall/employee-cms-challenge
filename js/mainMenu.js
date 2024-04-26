7eea7944a80e6120a2ac7e8c2c0c6c54cc276213
Alfred Wallace <contact@alfredwallace.me>
2024-04-26 15:51:49 -0700

    Added some basic SQL queries, unformatted

--- js/mainMenu.js
+++ js/mainMenu.js
@@ -1,5 +1,6 @@
 const inquirer = require("inquirer");
 
+
 const startQuestion = [
     {
         name: "main_choice",
@@ -17,31 +18,38 @@ const startQuestion = [
     }
 ];
 
-async function mainMenu() {
+async function mainMenu(db) {
     inquirer.prompt(startQuestion)
         .then((answer) => {
             console.log(`Answer was ${answer.main_choice}`)
             switch (answer.main_choice) {
                 case "View All Employees":
-                    console.log("select * from employees");
+                    db.query("select id, first_name from employees;",
+                        function (err, results) {
+                            console.log(err ? err : results);
+                        });
                     break;
                 case "Add Employee":
-                    console.log("insert into employees");
+                    //console.log("insert into employees");
                     break;
                 case "Update Employee Role":
-                    console.log("update employee roles");
+                    //console.log("update employee roles");
                     break;
                 case "View All Roles":
-                    console.log("select * from roles");
+                    db.query("select id, title, department_id, salary from roles;", function (err, results) {
+                        console.log(err ? err : results);
+                    });
                     break;
                 case "Add Role":
-                    console.log("insert into roles");
+                    //console.log("insert into roles");
                     break;
                 case "View All Departments":
-                    console.log("select * from departments");
+                    db.query("select id, name from departments;", function (err, results) {
+                        console.log(err ? err : results);
+                    });
                     break;
                 case "Add Department":
-                    console.log("insert into departments");
+                    //console.log("insert into departments");
                     break;
                 default:
                     throw new Error("Error when selecting main command!")
--- server.js
+++ server.js
@@ -1,7 +1,17 @@
-const mainMenu = require("./subMenus/mainMenu.js");
+const mainMenu = require("./js/mainMenu.js");
+const mysql = require("mysql2");
+const db = mysql.createConnection(
+    {
+        host: "localhost",
+        user: "root",
+        password: "admin",
+        database: "employees_db"
+    },
+    console.log("Connected to employees_db!")
+);
 
 async function Init() {
-    await mainMenu();
+    await mainMenu(db);
 }
 
 Init();
